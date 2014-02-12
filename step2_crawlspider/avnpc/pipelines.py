bfdc8fe237a283dab5180b40825009b458384c6f
AlloVince <allo.vince@gmail.com>
2014-02-12 15:37:55 +0800

    move output to download folder

--- step2_crawlspider/avnpc/pipelines.py
+++ step2_crawlspider/avnpc/pipelines.py
@@ -1,4 +1,5 @@
 #coding=utf-8
+import os
 import json
 import codecs 
 from urlparse import urlparse
@@ -6,10 +7,13 @@ from scrapy.exceptions import *
 from avnpc.items import AvnpcPostItem
 from avnpc import *
 
+
 class AvnpcPipeline(object):
     def process_item(self, item, spider):
         url = urlparse(item['url'].rstrip('/'))
         filename = 'index.json' if url.path == '/' else url.path.split("/")[-1] + '.json'
-        codecs.open(filename, 'wb', encoding='utf-8').write(json.dumps(dict(item), ensure_ascii=False))
+        filepath = os.path.dirname(__file__) + '/../download/' + filename
+        
+        codecs.open(filepath, 'wb', encoding='utf-8').write(json.dumps(dict(item), ensure_ascii=False))
         #raise CloseSpider('exit')
         return item
